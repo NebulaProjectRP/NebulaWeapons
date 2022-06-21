ace390dd083ccdfef0236a2792fa8755c46bdac6
Gonzalolog <gonzalolog@gmail.com>
2022-06-21 18:37:27 -0300

    Added weapon bases

---
+++ lua/weapons/nebula_nade/shared.lua
@@ -0,0 +1,7 @@
+SWEP.Base = "nebula_sck"
+SWEP.PrintName = "Nebula Nade"
+SWEP.Category = "NebulaRP"
+SWEP.Spawnable = true
+
+SWEP.ViewModel = "models/weapons/c_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
