38fd9bb357119746d6c2aee815c8879592b2de4b
Gonzalolog <gonzalolog@gmail.com>
2022-08-17 21:08:31 -0300

    Update sv_wep_modifier.lua

--- lua/autorun/server/sv_wep_modifier.lua
+++ lua/autorun/server/sv_wep_modifier.lua
@@ -5,10 +5,10 @@ net.Receive("NebulaWeapons:UpdateWeapon", function(l, ply)
     if not ply:IsSuperAdmin() then return end
     local wep = net.ReadString()
     local data = net.ReadTable()
-
     local wepTable = weapons.GetStored(wep)
+
     for k, v in pairs(data) do
-        if (istable(v)) then
+        if istable(v) then
             for i, d in pairs(v) do
                 wepTable[k][i] = d
             end
@@ -33,6 +33,7 @@ end)
 net.Receive("NebulaWeapons:DeleteWeapon", function(l, ply)
     if not ply:IsSuperAdmin() then return end
     local wep = net.ReadString()
+
     http.Post(NebulaAPI.HOST .. "weapons/upload", {
         class = wep,
     }, function(res)
@@ -48,22 +49,62 @@ local commands = {
     ["!buyammo"] = true,
     ["!buyuniammo"] = true,
     ["!buyallammo"] = true,
-    ["/buyammo"] = true,
-    ["/buyuniammo"] = true,
-    ["/buyallammo"] = true
 }
 
 hook.Add("PlayerSay", "NebulaRP.ReplenishAmmo", function(ply, text)
-    if (commands[text]) then
+    if commands[text] then
         local types = {}
+
         for k, v in pairs(ply:GetWeapons()) do
-            if (v.Primary and not types[v.Primary.Ammo]) then
+            if v.Primary and not types[v.Primary.Ammo] then
                 types[v.Primary.Ammo] = true
                 ply:GiveAmmo(100, v.Primary.Ammo)
             end
         end
+
         ply:addMoney(-1000)
         ply:PrintMessage(HUD_PRINTTALK, "You bought ammo for all your weapons!")
+
         return ""
     end
+end)
+
+timer.Simple(5, function()
+    DarkRP.declareChatCommand{
+        command = "buyammo",
+        description = "Buys ammo",
+        delay = 1.5
+    }
+
+    DarkRP.declareChatCommand{
+        command = "buyuniammo",
+        description = "Buys ammo",
+        delay = 1.5
+    }
+
+    DarkRP.declareChatCommand{
+        command = "buyallammo",
+        description = "Buys ammo",
+        delay = 1.5
+    }
+
+    local function buyAmmo(ply, args)
+        local types = {}
+
+        for k, v in pairs(ply:GetWeapons()) do
+            if v.Primary and not types[v.Primary.Ammo] then
+                types[v.Primary.Ammo] = true
+                ply:GiveAmmo(100, v.Primary.Ammo)
+            end
+        end
+
+        ply:addMoney(-1000)
+        ply:PrintMessage(HUD_PRINTTALK, "You bought ammo for all your weapons!")
+
+        return ""
+    end
+
+    DarkRP.defineChatCommand("buyuniammo", buyAmmo)
+    DarkRP.defineChatCommand("buyallammo", buyAmmo)
+    DarkRP.defineChatCommand("buyammo", buyAmmo)
 end)
