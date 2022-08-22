4ec913247d12b8862f76fc5e62700430e2de4bff
Gonzalolog <gonzalolog@gmail.com>
2022-08-22 11:26:41 -0300

    Update neb_missile_1.lua

--- lua/entities/neb_missile_1.lua
+++ lua/entities/neb_missile_1.lua
@@ -6,7 +6,7 @@ ENT.Spawnable = false
 ENT.AdminSpawnable = false
 ENT.MyModel = "models/weapons/tfa_cso/w_tank_missile.mdl"
 ENT.MyModelScale = 1
-ENT.Damage = 60
+ENT.Damage = 80
 ENT.Radius = 200
 
 if SERVER then
@@ -21,6 +21,7 @@ if SERVER then
         self:SetSolid(SOLID_VPHYSICS)
         self:DrawShadow(true)
         self:SetCollisionGroup(COLLISION_GROUP_NONE)
+        self:GetPhysicsObject():SetMass(1)
         self:SetHealth(1)
         self:SetModelScale(self.MyModelScale, 0)
         util.SpriteTrail(self, 0, Color(255, 255, 255), false, 7, 1, 0.1, 0.125, "trails/smoke.vmt")
@@ -33,7 +34,18 @@ if SERVER then
 
     function ENT:PhysicsCollide(data, physobj)
         local owent = self.Owner and self.Owner or self
-        util.BlastDamage(self, owent, self:GetPos(), self.Radius, self.Damage)
+        local filter = {}
+        for k, v in pairs(ents.FindInSphere(self:GetPos(), self.Radius)) do
+            if !v:IsPlayer() or filter[v] then continue end
+            filter[v] = true
+            local dmg = DamageInfo()
+            dmg:SetAttacker(owent)
+            dmg:SetInflictor(self)
+            dmg:SetDamage(self.Damage)
+            dmg:SetDamageType(DMG_BLAST)
+            v:TakeDamageInfo(dmg)
+        end
+        //util.BlastDamage(self, owent, self:GetPos(), self.Radius, self.Damage)
         local fx = EffectData()
         fx:SetOrigin(self:GetPos())
         fx:SetNormal(data.HitNormal)
@@ -47,3 +59,19 @@ if CLIENT then
         self:DrawModel()
     end
 end
+
+local ENT2 = table.Copy(ENT)
+ENT2.Base = "neb_missile_1"
+ENT2.Damage = 125
+ENT2.Radius = 200
+
+scripted_ents.Register(ENT2, "neb_missile_2")
+
+local ENT3 = table.Copy(ENT)
+ENT3.Base = "neb_missile_1"
+ENT3.Damage = 400
+ENT3.Radius = 200
+ENT3.MyModel = "models/weapons/tfa_cso/w_tank_missile.mdl"
+
+scripted_ents.Register(ENT3, "neb_missile_3")
+
